419cb4938daf6771f734eb41c79ebd5e9ddda698
samfreedgood <sam.freedgood@gmail.com>
2019-03-27 11:20:30 -0400

    add opacity to user text background, fix indenting

--- components/userContent/UserCamps.js
+++ components/userContent/UserCamps.js
@@ -7,122 +7,121 @@ const serverURL = Config.SERVER_HOST_URL
 const backgroundUrl = require('../../assets/background.jpg')
 
 export default class UserCamps extends Component {
-    state = {
-        data: [],
-        loaded: false
-    }
+  state = {
+    data: [],
+    loaded: false
+  }
 
-    componentDidMount = async () => {
-        try{
-            const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
-            const token = nonsense[0][1]
-            const userId = Number(nonsense[1][1])
+  componentDidMount = async () => {
+    try{
+      const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
+      const token = nonsense[0][1]
+      const userId = Number(nonsense[1][1])
 
-            const res = await Axios.get(serverURL + `/users/${userId}/camps`, {
-                headers: {
-                Authorization: 'Bearer ' + token //the token is a variable which holds the token
-                }
-            })
-            this.setState({
-                loaded: true,
-                data: res.data.userCamps
-            })
-        } catch(e) {
-            console.log(e)
+      const res = await Axios.get(serverURL + `/users/${userId}/camps`, {
+        headers: {
+          Authorization: 'Bearer ' + token //the token is a variable which holds the token
         }
+      })
+      this.setState({
+        loaded: true,
+        data: res.data.userCamps
+      })
+    } catch(e) {
+      console.log(e)
     }
+  }
 
-    componentDidUpdate = async () => {
-        try{
-            const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
-            const token = nonsense[0][1]
-            const userId = Number(nonsense[1][1])
+  componentDidUpdate = async () => {
+    try{
+      const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
+      const token = nonsense[0][1]
+      const userId = Number(nonsense[1][1])
 
-            const res = await Axios.get(serverURL + `/users/${userId}/camps`, {
-                headers: {
-                Authorization: 'Bearer ' + token //the token is a variable which holds the token
-                }
-            })
-            this.setState({
-                loaded: true,
-                data: res.data.userCamps
-            })
-        } catch(e) {
-            console.log(e)
+      const res = await Axios.get(serverURL + `/users/${userId}/camps`, {
+        headers: {
+          Authorization: 'Bearer ' + token //the token is a variable which holds the token
         }
+      })
+      this.setState({
+        loaded: true,
+        data: res.data.userCamps
+      })
+    } catch(e) {
+      console.log(e)
     }
+  }
 
-    renderUserCamps = (campData) => {
-        const campList = campData.map((el, key) => {
-            return (
-                <RN.View 
-                        style={styles.campContainer}
-                        id={el.campCode}
-                        key={key} >
-                    <RN.Text style={styles.campName}>
-                        {el.name}
-                    </RN.Text>
-                    <RN.Button
-                        title={"Delete"}
-                        onPress={ () => this.deleteCamp(el.id) }
-                    />
-                    <RN.Text style={styles.campDescription}>
-                        {el.description}
-                    </RN.Text>
-    
-                </RN.View>
-            )
-        })
-        return campList
-    }
+  renderUserCamps = (campData) => {
+    const campList = campData.map((el, key) => {
+      return (
+        <RN.View 
+            style={styles.campContainer}
+            id={el.campCode}
+            key={key} >
+          <RN.Text style={styles.campName}>
+            {el.name}
+          </RN.Text>
+          <RN.Button
+              title={"Delete"}
+              onPress={ () => this.deleteCamp(el.id) } />
+          <RN.Text style={styles.campDescription}>
+            {el.description}
+          </RN.Text>
+        </RN.View>
+      )
+    })
+    return campList
+  }
 
-    deleteCamp = async (campId) => {
-        try {
-            const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
-            const token = nonsense[0][1]
-            const userId = Number(nonsense[1][1])
+  deleteCamp = async (campId) => {
+    try {
+      const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
+      const token = nonsense[0][1]
+      const userId = Number(nonsense[1][1])
 
-            const res = await Axios.delete(serverURL + `/users/${userId}/camps/${campId}`, {
-                headers: {
-                Authorization: 'Bearer ' + token //the token is a variable which holds the token
-                }
-            })
-            RN.Alert.alert(res.data.message)
-        } catch (e) {
-            console.log(e)
+      const res = await Axios.delete(serverURL + `/users/${userId}/camps/${campId}`, {
+        headers: {
+          Authorization: 'Bearer ' + token //the token is a variable which holds the token
         }
+      })
+      RN.Alert.alert(res.data.message)
+    } catch (e) {
+      console.log(e)
     }
+  }
 
-    render() {
-        return(
-            <RN.SafeAreaView>
-                <RN.ImageBackground 
-                    source = {backgroundUrl}
-                    style = {styles.backgroundImg} >
-                    <RN.ScrollView>
-                        {this.state.loaded && this.renderUserCamps(this.state.data)}
-                    </RN.ScrollView>
-                </RN.ImageBackground>
-            </RN.SafeAreaView>
-        )
-    }
+  render() {
+    return(
+      <RN.SafeAreaView>
+        <RN.ImageBackground 
+            source = {backgroundUrl}
+            style = {styles.backgroundImg} >
+          <RN.ScrollView style = {{paddingBottom: 20}}>
+            {this.state.loaded && this.renderUserCamps(this.state.data)}
+          </RN.ScrollView>
+        </RN.ImageBackground>
+      </RN.SafeAreaView>
+    )
+  }
 }
 
 const styles = RN.StyleSheet.create({
-    campContainer: {
-        padding: 2,
-    },
-    campName: {
-        textAlign: "center",
-        fontSize: 20,
-        fontWeight: "600",
-    },
-    backgroundImg: {
-        justifyContent: "center",
-        width: RN.Dimensions.get("window").width,
-        height: RN.Dimensions.get("window").height
-    },
-    campDescription: {
+  campContainer: {
+    padding: 2,
+    backgroundColor: "rgba(200, 200, 200, 0.7)",
+  },
+  campName: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  backgroundImg: {
+    justifyContent: "center",
+    width: RN.Dimensions.get("window").width,
+    height: RN.Dimensions.get("window").height
+  },
+  campDescription: {
 
-    }
+  }
 })
--- components/userContent/UserParks.js
+++ components/userContent/UserParks.js
@@ -7,106 +7,105 @@ const serverURL = Config.SERVER_HOST_URL
 const backgroundUrl = require('../../assets/background.jpg')
 
 export default class UserParks extends Component {
-    state = {
-        data: [],
-        loaded: false
-    }
+  state = {
+    data: [],
+    loaded: false
+  }
 
-    componentDidMount = async () => {
-        try{
-            const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
-            const token = nonsense[0][1]
-            const userId = Number(nonsense[1][1])
+  componentDidMount = async () => {
+    try{
+      const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
+      const token = nonsense[0][1]
+      const userId = Number(nonsense[1][1])
 
-            const res = await Axios.get(serverURL + `/users/${userId}/parks`, {
-                headers: {
-                Authorization: 'Bearer ' + token //the token is a variable which holds the token
-                }
-            })
-            this.setState({
-                loaded: true,
-                data: res.data.userParks
-            })
-        } catch(e) {
-            console.log(e)
+      const res = await Axios.get(serverURL + `/users/${userId}/parks`, {
+        headers: {
+          Authorization: 'Bearer ' + token //the token is a variable which holds the token
         }
+      })
+      this.setState({
+        loaded: true,
+        data: res.data.userParks
+      })
+    } catch(e) {
+      console.log(e)
     }
+  }
 
-    deletePark = async (parkId) => {
-        try {
-            const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
-            const token = nonsense[0][1]
-            const userId = Number(nonsense[1][1])
+  deletePark = async (parkId) => {
+    try {
+      const nonsense = await RN.AsyncStorage.multiGet(['token', 'userId'])
+      const token = nonsense[0][1]
+      const userId = Number(nonsense[1][1])
 
-            const res = await Axios.delete(serverURL + `/users/${userId}/parks/${parkId}`, {
-                headers: {
-                Authorization: 'Bearer ' + token //the token is a variable which holds the token
-                }
-            })
-            RN.Alert.alert(res.data.message)
-            this.setState({
-                deleted: true
-            })
-        } catch (e) {
-            console.log(e)
+      const res = await Axios.delete(serverURL + `/users/${userId}/parks/${parkId}`, {
+        headers: {
+          Authorization: 'Bearer ' + token //the token is a variable which holds the token
         }
+      })
+      RN.Alert.alert(res.data.message)
+      this.setState({
+        deleted: true
+      })
+    } catch (e) {
+      console.log(e)
     }
+  }
 
-    renderUserParks = (parkData) => {
-        const parkList = parkData.map((el, key) => {
-            return (
-              <RN.View 
-                  style={styles.parkContainer}
-                  id={el.parkCode}
-                  key={key} >
-                <RN.Text style={styles.parkName}>
-                    {el.name}
-                </RN.Text>
+  renderUserParks = (parkData) => {
+    const parkList = parkData.map((el, key) => {
+      return (
+        <RN.View 
+            style={styles.parkContainer}
+            id={el.parkCode}
+            key={key} >
+          <RN.Text style={styles.parkName}>
+            {el.name}
+          </RN.Text>
+          <RN.Button
+              title={"Delete"}
+              onPress={ () => this.deletePark(el.id) } />
+          <RN.Text style={styles.parkDescription}>
+            {el.description}
+          </RN.Text>
+        </RN.View>
+      )
+    })
+    return parkList
+  }
 
-                <RN.Button
-                    title={"Delete"}
-                    onPress={ () => this.deletePark(el.id) } 
-                />
-                <RN.Text style={styles.parkDescription}>
-                    {el.description}
-                </RN.Text>
-              </RN.View>
-            )
-        })
-        return parkList
-    }
-
-    render() {
-        return(
-            <RN.SafeAreaView>
-                <RN.ImageBackground 
-                    source = {backgroundUrl}
-                    style = {styles.backgroundImg} >
-                    <RN.ScrollView>
-                        {this.state.loaded && this.renderUserParks(this.state.data)}
-                    </RN.ScrollView>
-                </RN.ImageBackground>
-            </RN.SafeAreaView>
-        )
-    }
+  render() {
+    return(
+      <RN.SafeAreaView>
+        <RN.ImageBackground 
+            source = {backgroundUrl}
+            style = {styles.backgroundImg} >
+          <RN.ScrollView style = {{paddingBottom: 20}}>
+            {this.state.loaded && this.renderUserParks(this.state.data)}
+          </RN.ScrollView>
+        </RN.ImageBackground>
+      </RN.SafeAreaView>
+    )
+  }
 }
 
 const styles = RN.StyleSheet.create({
-    parkContainer: {
-        padding: 2,
-    },
-    parkName: {
-        textAlign: "center",
-        fontSize: 20,
-        fontWeight: "600",
-    },
-    backgroundImg: {
-        justifyContent: "center",
-        width: RN.Dimensions.get("window").width,
-        height: RN.Dimensions.get("window").height,
-        opacity: 50
-    },
-    parkDescription: {
+  parkContainer: {
+    padding: 2,
+    backgroundColor: "rgba(200, 200, 200, 0.7)",
+  },
+  parkName: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  backgroundImg: {
+    justifyContent: "center",
+    width: RN.Dimensions.get("window").width,
+    height: RN.Dimensions.get("window").height,
+    opacity: 50
+  },
+  parkDescription: {
 
-    }
+  }
 })
